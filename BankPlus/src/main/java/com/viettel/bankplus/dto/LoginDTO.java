b7f0e6078feb08e4af248f30aeb07e7bd13e5ec3
tuyendt862 <tuyendt862@gmail.com>
2019-07-30 00:10:38 +0700

    update

--- BankPlus/src/main/java/com/viettel/bankplus/dto/LoginDTO.java
+++ BankPlus/src/main/java/com/viettel/bankplus/dto/LoginDTO.java
@@ -1,16 +1,12 @@
 package com.viettel.bankplus.dto;
 
-import javax.validation.constraints.Max;
-
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class LoginDTO {
 
 	@NotEmpty(message = "NotEmpty")
-	@Max(value = 12, message = "toi da 12 ky tu")
 	private String userName;
 	@NotEmpty(message = "NotEmpty")
-	@Max(value = 6, message = "toi da 6 ky tu")
 	private String passWord;
 
 	public String getUserName() {
